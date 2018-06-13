3da27c62e4773b65cc197bac4f83cd49df332b93
Brian M. Palma <brian.palma@stablekernel.com>
2018-06-13 14:56:08 -0400

    Add wait for presented / dimissed tests
    - Add base view controller TestViewController to handle lookup of bundle for test view controller xibs
    - Add WaitViewController and ModalViewController to test presentation / dismissal

---
+++ ArmoryTests/View Controllers/ModalViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ModalViewController.swift
+//  ArmoryTests
+//
+//  Created by Brian Palma on 6/13/18.
+//  Copyright © 2018 stablekernel. All rights reserved.
+//
+
+import UIKit
+
+protocol ModalViewControllerDelegate {
+    func done(_ viewController: ModalViewController)
+}
+
+class ModalViewController: TestViewController {
+
+    // MARK: - Public
+
+    var doneButton: UIBarButtonItem {
+        return navigationItem.leftBarButtonItem!
+    }
+
+    var delegate: ModalViewControllerDelegate?
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done(_:)))
+    }
+
+    // MARK: - IBActions
+
+    @IBAction func done(_ sender: UIBarButtonItem) {
+        delegate?.done(self)
+    }
+}
