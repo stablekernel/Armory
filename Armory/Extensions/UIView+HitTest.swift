1a1d5c33225e872e667c0e422e7faed39c64dd0d
Brian M. Palma <brian.palma@stablekernel.com>
2018-06-14 09:54:30 -0400

    Fix and simplify logic in isSkippedDuringHitTest

--- Armory/Extensions/UIView+HitTest.swift
+++ Armory/Extensions/UIView+HitTest.swift
@@ -13,7 +13,7 @@ public extension UIView {
     var isSkippedDuringHitTest: Bool {
         // From hitTest(_:with:) docs:
         // This method ignores view objects that are hidden, that have disabled user interactions, or have an alpha level less than 0.01.
-        return !isHidden && isUserInteractionEnabled && alpha > 0.01 ? false : true
+        return isHidden || !isUserInteractionEnabled || alpha < 0.01
     }
 }
 
