c42d3bb4972053e0e3f4a469c5b505a25982af98
Cameron Smith <cabotmoose@gmail.com>
2018-06-20 09:33:24 -0400

    Add UIAlertController and UIActionSheet tests

---
+++ ArmoryTests/AlertControllerTests.swift
@@ -0,0 +1,80 @@
+//
+//  AlertControllerTests.swift
+//  ArmoryTests
+//
+//  Created by Cameron Smith on 6/12/18.
+//  Copyright © 2018 stablekernel. All rights reserved.
+//
+
+import XCTest
+import Foundation
+import UIKit
+
+@testable import Armory
+
+class AlertControllerTests: XCTestCase, VCTest {
+    
+    var viewController: AlertViewController!
+    
+    override func setUp() {
+        super.setUp()
+        
+        viewController = AlertViewController()
+        build()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    // MARK: - UIAlertController Tests
+    
+    func testAlertControllerIsPresented() {
+        viewController.setupAlertController(style: .alert, title: "Alert Title")
+        
+        tap(viewController.showAlertButton)
+        
+        let _: UIAlertController = waitForPresentedViewController()
+    }
+    
+    func testTypingInAlertControllerTextField() {
+        viewController.setupAlertController(style: .alert, title: "Alert Title")
+        
+        let alertController = viewController.alertController
+        
+        alertController?.addTextField(configurationHandler: nil)
+        
+        let text = "Lorem ipsum"
+        type(alertController!.textFields!.first!, text: text)
+        
+        tap(viewController.showAlertButton)
+        
+        XCTAssertEqual(alertController!.textFields!.first!.text!, text)
+    }
+    
+    func testCorrectAlertActionHandlerIsCalled() {
+        let blueButton = UIAlertAction(title: "Blue", style: .default) { action in
+            self.viewController.view.backgroundColor = .blue
+        }
+        
+        let redButton = UIAlertAction(title: "Red", style: .default) { action in
+            self.viewController.view.backgroundColor = .red
+        }
+        
+        viewController.setupAlertController(style: .alert, title: "Change Background", message: nil, actions: [blueButton, redButton])
+        
+        tapButton(withTitle: "Red", fromAlertController: viewController.alertController)
+        
+        XCTAssertEqual(viewController.view.backgroundColor, UIColor.red)
+    }
+    
+    // MARK: - ActionSheet Tests
+    
+    func testActionSheetAppears() {
+        viewController.setupAlertController(style: .actionSheet, title: "Alert Title")
+        
+        tap(viewController.showAlertButton)
+        
+        let _: UIAlertController = waitForPresentedViewController()
+    }
+}
