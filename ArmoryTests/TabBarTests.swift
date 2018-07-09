a69c838781a0346b780ed9e775f8f402860c8598
Cameron Smith <cabotmoose@gmail.com>
2018-07-09 11:06:11 -0400

    Fix whitespace indentation

--- ArmoryTests/TabBarTests.swift
+++ ArmoryTests/TabBarTests.swift
@@ -12,35 +12,35 @@ import UIKit
 @testable import Armory
 
 class TabBarTests: XCTestCase, VCTestSetup {
-
+    
     // MARK: - VCTestSetup
-
+    
     var viewController: TabBarViewController!
-
+    
     // MARK: - Set Up / Tear Down
-
+    
     override func setUp() {
         super.setUp()
-
+        
         viewController = TabBarViewController()
-
+        
         build()
     }
-
+    
     override func tearDown() {
         viewController = nil
-
+        
         super.tearDown()
     }
-
+    
     // MARK: - Tests
-
+    
     func testSelectTabByIndex() {
         try! selectTab(atIndex: 0, fromTabBar: viewController.tabBar)
-
+        
         XCTAssertEqual(viewController.tabOneItem, viewController.tabBar.selectedItem)
     }
-
+    
     func testSelectTabByIndexLessThanZero() {
         do {
             try selectTab(atIndex: -1, fromTabBar: viewController.tabBar)
@@ -51,7 +51,7 @@ class TabBarTests: XCTestCase, VCTestSetup {
             XCTFail("Unexpected error: \(error.localizedDescription)")
         }
     }
-
+    
     func testSelectTabByIndexGreaterThanTabCount() {
         do {
             let tabCount = viewController.tabBar.items?.count ?? 0
@@ -63,13 +63,13 @@ class TabBarTests: XCTestCase, VCTestSetup {
             XCTFail("Unexpected error: \(error.localizedDescription)")
         }
     }
-
+    
     func testSelectTabByTitle() {
         try! selectTab(withTitle: "Index 1", fromTabBar: viewController.tabBar)
-
+        
         XCTAssertEqual(viewController.tabTwoItem, viewController.tabBar.selectedItem)
     }
-
+    
     func testSelectTabByTitleFailure() {
         do {
             try selectTab(withTitle: "Missing Title", fromTabBar: viewController.tabBar)
@@ -80,12 +80,12 @@ class TabBarTests: XCTestCase, VCTestSetup {
             XCTFail("Unexpected error: \(error.localizedDescription)")
         }
     }
-
+    
     func testSelectTabByTitleMultipleMatchesFailure() {
         let title = "Tab One"
-
+        
         viewController.tabBar.items!.forEach { $0.title = title }
-
+        
         do {
             try selectTab(withTitle: title, fromTabBar: viewController.tabBar)
             XCTFail("Expected test to throw error")
@@ -95,13 +95,13 @@ class TabBarTests: XCTestCase, VCTestSetup {
             XCTFail("Unexpected error: \(error.localizedDescription)")
         }
     }
-
+    
     func testSelectTabByImage() {
         try! selectTab(withImage: UIImage.close(), fromTabBar: viewController.tabBar)
-
+        
         XCTAssertEqual(viewController.tabTwoItem, viewController.tabBar.selectedItem)
     }
-
+    
     func testSelectTabByImageFailure() {
         do {
             try selectTab(withImage: UIImage(), fromTabBar: viewController.tabBar)
